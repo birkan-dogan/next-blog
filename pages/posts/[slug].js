85a01a2b7d24ed72b9f290307c9efa812a7bc2f3
birkan <b.561birkan@gmail.com>
2022-11-10 22:08:29 +0300

    structure of pages is done

---
+++ pages/posts/[slug].js
@@ -0,0 +1,12 @@
+const PostDetailPage = () => {
+  return (
+    <div>
+      <h1>
+        url will be localhost:3000/getting-started-with-nextjs and the page will
+        displayed
+      </h1>
+    </div>
+  );
+};
+
+export default PostDetailPage;
---
+++ pages/posts/index.js
@@ -0,0 +1,9 @@
+const AllPostsPage = () => {
+  return (
+    <div>
+      <h1>url will be localhost:3000//posts and this page will be displayed</h1>
+    </div>
+  );
+};
+
+export default AllPostsPage;
