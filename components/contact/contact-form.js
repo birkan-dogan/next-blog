ba7052756ae6a99a016cd326881c092271150a59
birkan <b.561birkan@gmail.com>
2022-11-14 22:43:29 +0300

    sending data from the client to the api route (not included storing yet)

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,53 @@
 import classes from "./contact-form.module.css";
 
+import { useRef } from "react";
+
 const ContactForm = () => {
+  const emailInputRef = useRef();
+  const nameInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = {
+      email: enteredEmail,
+      name: enteredName,
+      message: enteredFeedback,
+    };
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can we help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" required ref={emailInputRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required ref={nameInputRef} />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea id="message" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
